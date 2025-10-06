72958ed6080d632a87a41a50d0f89cdbe40dd598
Thi Lam <z.thiilam@gmail.com>
2025-10-06 14:17:03 -0500

    fix: shrink menu when scroll down

--- assets/js/main.js
+++ assets/js/main.js
@@ -148,3 +148,26 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 });
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#header');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+});
